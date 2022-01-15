b9f3b9ae337c2c1e786bbbc1fecb54d77db2893b
UZA97 <81206899+UZA97@users.noreply.github.com>
2022-01-15 22:01:24 +0900

    # Rename

---
+++ Chick/Chick/Assets/Scripts/GoogleManager.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using GooglePlayGames;
+using GooglePlayGames.BasicApi;
+using Firebase.Auth;
+
+public class GoogleManager : MonoBehaviour
+{
+    Text LogText;
+
+    private void Start()
+    {
+        PlayGamesPlatform.InitializeInstance(new PlayGamesClientConfiguration.Builder().RequestIdToken().RequestEmail().Build());
+        PlayGamesPlatform.DebugLogEnabled = true;
+        PlayGamesPlatform.Activate();
+        LogIn();
+    }
+
+    public void LogIn()
+    {
+        Social.localUser.Authenticate((bool success) =>
+        {
+            if (success) Debug.Log(Social.localUser.id + " \n " + Social.localUser.userName);
+            //else Debug.Log("구글 로그인 실패");
+        });
+    }
+
+    public void TryGoogleLogin()
+    {
+        if (!Social.localUser.authenticated) // 로그인 되어 있지 않다면
+        {
+            Social.localUser.Authenticate(success => // 로그인 시도
+            {
+                if (success) // 성공하면
+                {
+                    Debug.Log("Success");
+                    StartCoroutine(TryFirebaseLogin()); // Firebase Login 시도
+                }
+                else // 실패하면
+                {
+                    Debug.Log("Fail");
+                }
+            });
+        }
+    }
+
+    public void LogOut()
+    {
+        ((PlayGamesPlatform)Social.Active).SignOut();
+       Debug.Log("구글 로그아웃");
+    }
+    IEnumerator TryFirebaseLogin()
+    {
+        while (string.IsNullOrEmpty(((PlayGamesLocalUser)Social.localUser).GetIdToken()))
+            yield return null;
+        string idToken = ((PlayGamesLocalUser)Social.localUser).GetIdToken();
+  
+        Credential credential = GoogleAuthProvider.GetCredential(idToken, null);
+        DBManager._instance.mAuth.SignInWithCredentialAsync(credential).ContinueWith(task => {
+            if (task.IsCanceled)
+            {
+                Debug.LogError("SignInWithCredentialAsync was canceled.");
+                return;
+            }
+            if (task.IsFaulted)
+            {
+                Debug.LogError("SignInWithCredentialAsync encountered an error: " + task.Exception);
+                return;
+            }
+ 
+            Debug.Log("Success!");
+        });
+    }
+}
---
+++ Chick/Chick/Assets/Scripts/SceneData.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneData : MonoBehaviour
+{
+    public static SceneData _instance;
+    public Text userName;
+    public string username;
+    private void Awake()
+    {
+        Application.targetFrameRate = 60;   // 프레임   60고정
+        if(_instance == null) {
+            _instance = this;
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+    private void Update()
+    {
+        if(SceneManager.GetActiveScene().name == "Login") {
+            userName.text = "반갑습니다 " + username.ToString() + "님";
+        }
+    }
+}
