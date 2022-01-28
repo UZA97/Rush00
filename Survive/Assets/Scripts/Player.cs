849dbed779eefb4cb3908994f295ef70bcf2a571
UZA97 <81206899+UZA97@users.noreply.github.com>
2022-01-29 00:56:08 +0900

    .

---
+++ Survive/Assets/Scripts/Player.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (PlayerController))]
+[RequireComponent (typeof (GunController))]
+public class Player : MonoBehaviour
+{
+    PlayerController controller;
+    Camera viewCamera;
+    GunController gunController;
+    [SerializeField]
+    private float fMoveSpeed = 5;
+    void Start()
+    {
+        controller = GetComponent<PlayerController>();
+        gunController = GetComponent<GunController>();
+        viewCamera = Camera.main;
+    }
+
+    void Update()
+    {
+        Vector3 moveInput = new Vector3(Input.GetAxisRaw("Horizontal"),0,Input.GetAxisRaw("Vertical"));
+        Vector3 moveVelocity = moveInput.normalized * fMoveSpeed;
+        controller.Move(moveVelocity);
+
+        Ray ray = viewCamera.ScreenPointToRay(Input.mousePosition);
+        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+        float rayDistance;
+
+        if(groundPlane.Raycast(ray,out rayDistance)) {
+            Vector3 point = ray.GetPoint(rayDistance);
+            Debug.DrawLine(ray.origin,point,Color.red);
+            controller.LookAt(point);
+        }
+
+        if(Input.GetMouseButton(0)) {
+            gunController.Shoot();
+        }
+    }
+}
