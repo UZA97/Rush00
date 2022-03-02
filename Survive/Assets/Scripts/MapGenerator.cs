db04a8d8397551ff461be0b21b7fb06d98f3d7dd
UZA97 <81206899+UZA97@users.noreply.github.com>
2022-03-02 12:59:15 +0900

    # Using Floodfill & Fisher-Yates Algorithm

--- Survive/Assets/Scripts/MapGenerator.cs
+++ Survive/Assets/Scripts/MapGenerator.cs
@@ -4,17 +4,39 @@ using UnityEngine;
 
 public class MapGenerator : MonoBehaviour
 {
+    public struct Coord
+    {
+        public int x;
+        public int y;
+        public Coord(int _x, int _y)
+        {
+            x = _x;
+            y = _y;
+        }
+        public static bool operator ==(Coord c1, Coord c2)
+        {
+            return c1.x == c2.x && c1.y == c2.y;
+        }
+        public static bool operator !=(Coord c1, Coord c2)
+        {
+            return !(c1==c2);
+        }
+    }
+
     public Transform tilePrefab;
     public Transform obstaclePrefab;
     public Vector2 mapSize;
 
     [Range(0,1)]
     public float outlinePercent;
+    [Range(0,1)]
+    public float obstaclePercent;
 
     List<Coord> allTileCoords;
     Queue<Coord> shuffledTileCoords;
 
     public int seed = 10;
+    Coord mapCenter;
     void Start()
     {
         GenerateMap();
@@ -29,7 +51,7 @@ public class MapGenerator : MonoBehaviour
         }
 
         shuffledTileCoords = new Queue<Coord>(Utility.ShuffleArray(allTileCoords.ToArray(), seed));
-
+        mapCenter = new Coord((int)(mapSize.x*0.5), (int)(mapSize.y*0.5));
         string holderName = "Generated Map";
         if(transform.Find(holderName)) {
             DestroyImmediate(transform.Find(holderName).gameObject);   // 에디터에서 호출하므로
@@ -44,13 +66,67 @@ public class MapGenerator : MonoBehaviour
                 newTile.parent = mapHolder;
             }
         }
-        int obstacleCount = 10;
-        for (int i = 0; i<obstacleCount; i++) {
+
+        // 2차원 bool선언
+        bool[,] obstacleMap = new bool[(int)mapSize.x,(int)mapSize.y];
+
+        int obstacleCount = (int)(mapSize.x*mapSize.y*obstaclePercent);
+        int currentObstacleCount = 0;
+        for (int i = 0; i < obstacleCount; i++) {
             Coord randomCoord = GetRandomCoord();
-            Vector3 obstaclePosition = CoordToPosition(randomCoord.x, randomCoord.y);
-            Transform newObstacle = Instantiate(obstaclePrefab, obstaclePosition+ Vector3.up * 0.5f, Quaternion.identity) as Transform;
-            newObstacle.parent = mapHolder;
+            obstacleMap[randomCoord.x,randomCoord.y] = true;
+            currentObstacleCount++;
+            if(randomCoord != mapCenter  && MapIsFullyAccessible(obstacleMap, currentObstacleCount))    // Map 중앙을 비움(Create Player)
+            {
+                Vector3 obstaclePosition = CoordToPosition(randomCoord.x, randomCoord.y);
+
+                Transform newObstacle = Instantiate(obstaclePrefab, obstaclePosition+ Vector3.up * 0.5f, Quaternion.identity) as Transform;
+                newObstacle.parent = mapHolder;
+            }
+            else
+            {
+                obstacleMap[randomCoord.x,randomCoord.y] = false;
+                currentObstacleCount--;
+            }
         }
+    }
+
+    bool MapIsFullyAccessible(bool[,] obstacleMap, int currentObstacleCount)
+    {
+        bool[,] mapFlags = new bool[obstacleMap.GetLength(0),obstacleMap.GetLength(1)];
+        Queue<Coord> queue = new Queue<Coord>();
+        queue.Enqueue(mapCenter);
+        mapFlags[mapCenter.x,mapCenter.y] = true;
+        
+        int accessibleTileCount = 1;
+
+        while(queue.Count>0)
+        {
+            Coord tile = queue.Dequeue();
+
+            for(int x = -1; x<=1; x++)
+            {
+                for(int y = -1; y<=1; y++)
+                {
+                    int neighbourX = tile.x +x;
+                    int neighbourY = tile.y +y;
+                    if(x==0 || y ==0)
+                    {
+                        if(neighbourX >= 0 && neighbourX < obstacleMap.GetLength(0)&& neighbourY >= 0 && neighbourY<obstacleMap.GetLength(1))
+                        {
+                            if(!mapFlags[neighbourX,neighbourY] && !obstacleMap[neighbourX,neighbourY])
+                            {
+                                mapFlags[neighbourX,neighbourY] = true;
+                                queue.Enqueue(new Coord(neighbourX,neighbourY));
+                                accessibleTileCount++;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        int targetAccessibleTileCOunt = (int)(mapSize.x * mapSize.y - currentObstacleCount);
+        return targetAccessibleTileCOunt == accessibleTileCount;
     }
     Vector3 CoordToPosition(int x, int y)
     {
@@ -61,15 +137,5 @@ public class MapGenerator : MonoBehaviour
         Coord randomCoord = shuffledTileCoords.Dequeue();
         shuffledTileCoords.Enqueue(randomCoord);
         return randomCoord;
-    }
-    public struct Coord
-    {
-        public int x;
-        public int y;
-        public Coord(int _x, int _y)
-        {
-            x = _x;
-            y = _y;
-        }
     }
 }
